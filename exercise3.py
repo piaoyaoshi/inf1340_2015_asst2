3d28cafaed225d194c300acf06e1487d98742c1f
piaoyaoshi <piaoyao.shi@mail.utoronto.ca>
2015-11-02 16:59:29 -0500

    ex3_version1_1102

--- exercise3.py
+++ exercise3.py
@@ -23,23 +23,52 @@ def union(table1, table2):
     :raises: MismatchedAttributesException:
         if tables t1 and t2 don't have the same attributes
     """
-    return []
+
+    result = table1[:]
+    # same number of columns, the columns have the same names in the same order
+    if table1[0] != table2[0]:
+        raise MismatchedAttributesException
+    else:
+        for record in table2[1:]:  # order matters in a list; this line will not execute if table2 only has a header.
+            if record not in result:
+                result.append(record)
+    return result
 
 
 def intersection(table1, table2):
     """
-    Describe your function
+    Return a new_table contains rows that appear both in table1 and table2.
 
     """
-    return []
+
+    result = []
+
+    if table1[0] != table2[0]:
+        raise MismatchedAttributesException
+    else:
+        for record in table1:
+            # headers included
+            if record in table2:
+                result.append(record)
+    return result
 
 
 def difference(table1, table2):
     """
-    Describe your function
+    Return a new_table contains rows that appear in table1 but not in table2.
 
     """
-    return []
+
+    result = [table1[0]]
+    if table1[0] != table2[0]:
+        raise MismatchedAttributesException
+    else:
+        for record in table1:
+            # headers included
+            if record not in table2:
+                result.append(record)
+    return result
+
 
 
 #####################
