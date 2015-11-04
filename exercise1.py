736e183217c04dfc0d5cd8844f754fc09f001573
piaoyaoshi <piaoyao.shi@mail.utoronto.ca>
2015-11-04 16:24:19 -0500

    ex1_v2_1104_check_word_without_vowel

--- exercise1.py
+++ exercise1.py
@@ -12,19 +12,37 @@ __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
 
-def pig_latin_translator(english):
-    english = english.lower()
-    result = ""
-    vowel = "a,e,i,o,u"
-    if english[0] is vowel:
-        return result + "yay"
-    while english[0] is not vowel:
-        english[0] += (english[1:] + english[0])
-        if english[0] == vowel:
-            print result + "ay"
+def pig_latinify(word):
+    """
+    Return a Pig Latin word based on the word given.
+    If a word begins with a vowel, append 'yay' to the end of the word;
+    If the word begins with a consonant, remove all the consonants from the beginning up to the first vowel and
+    append them to the end of the word and append 'ay' to the end of the word.
+
+    Parameter passed in has to be an English word.
+
+    :param : word: a string
+    :return: a new word : a string
+    :raises:
+
+    """
+
+    result = ''
+    vowel_index = -1
+
+    for i in range(len(word)):
+        if word[i] in 'aeiouAEIOU':
+            vowel_index = i
+            break
+
+    # now we have vowel_i
+    if len(word) > 0 and vowel_index >= -1:
+        if vowel_index == 0:
+            result = word + 'yay'
+        elif vowel_index == -1:
+            result = word + 'ay'
         else:
-            return ""
-print pig_latin_translator("test")
+            result = word[vowel_index:] + word[0:vowel_index] + 'ay'
 
+    return result
 
-def pig_latin_translator():
