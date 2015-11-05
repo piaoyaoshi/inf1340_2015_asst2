1295bd2f0fc57f174c179f53a23b0e00323790b8
piaoyaoshi <piaoyao.shi@mail.utoronto.ca>
2015-11-04 21:45:54 -0500

    ex2_v3_1104_add_comments

--- exercise2.py
+++ exercise2.py
@@ -15,24 +15,26 @@ __license__ = "MIT License"
 def find(input_string, substring, start, end):
     """
     Return the lowest index in input_string where substring is found,
-    such that substring is contained within input_string[start:end]. required
-    arguments start and end are interpreted as in slice notation.
+    such that substring is wholly contained in input_string[start:end].
+    Arguments start and end are required and interpreted as in slice notation.
 
-    Return -1 on failure
+    Return -1 if substring is not found.
 
-    :param : input_string: a string
-             substring:    a string
-             start:        an int
-             end:          an int
-    :return: lowest i :    an int
-    :raises:
+    :param :input_string: a string
+            substring:    a string
+            start:        an int
+            end:          an int
+    :return:lowest i :    an int
+    :raises:none
 
     """
 
-    s = input_string[start:end]
-    for i in range(0, len(s) - len(substring) + 1):
-        if s[i: i + len(substring)] == substring:
+    target_str = input_string[start:end]                      # use standard slice notation.
+
+    for i in range(0, len(target_str) - len(substring) + 1):  # no need to check remaining str if length < substring
+        if target_str[i: i + len(substring)] == substring:    # compare strings in whole.
             return i + len(input_string[:start])
+
     return -1
 
 
@@ -40,25 +42,28 @@ def multi_find(input_string, substring, start, end):
     """(str, str, num, num) --> str
     Return all indices in the input_string[start: end] where the substring is found.
     Indices are displayed in a string, and separated by commas.
-    Return -1 if the substring is not found.
+    Arguments start and end are required and interpreted as in slice notation.
 
+    Return -1 if the substring is not found.
 
     :param :input_string: a string
             substring:    a string
-            start:        a number
-            end:          a number
+            start:        an int
+            end:          an int
     :return:result :      a string
-    :raises:
+    :raises:none
 
     """
-    result = ""
 
-    s = input_string[start:end]
-    for i in range(0, len(s) - len(substring) + 1):
-        if s[i: i + len(substring)] == substring:
-            result = result + str(i + len(input_string[:start])) + ','
+    result = ""
+    target_str = input_string[start:end]
+
+    for i in range(0, len(target_str) - len(substring) + 1):
+        if target_str[i: i + len(substring)] == substring:
+            position = str(i + len(input_string[:start]))
+            result += position + ','
 
     if len(result) > 0:
-        return result[:-1]
+        return result[:-1]   # do not include the last comma.
     else:
         return result
